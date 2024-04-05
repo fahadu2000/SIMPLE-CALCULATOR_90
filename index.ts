ab100308c7215bea6e8e45ca1e338740d62195e1
fahadu990 <mohammadfahad592@gmail.com>
2024-04-05 17:20:02 +0500

    check my simple calculator

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your frist number", type: "number", name: "fristNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+      message: "select one of operators to perform action ",
+      type: "list",
+      name: "operator",
+      choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+  ]);
+  // conditional statement 
+  if (answer.operator === "ADDITION"){
+      console.log(answer.fristNumber + answer.secondNumber);
+  }
+  else if(answer.operator === "SUBTRACTION"){
+      console.log(answer.fristNumber - answer.secondNumber);
+  }
+  else if(answer.operator === "MULTIPLICATION"){
+      console.log(answer.fristNumber * answer.secondNumber);
+  }
+  else if(answer.operator === "DIVISION"){
+      console.log(answer.fristNumber / answer.secondNumber);
+  }
+  else {"Please select correct operator"}
+  
